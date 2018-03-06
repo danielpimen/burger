a5695e5dc102b76e490a57ef8f0c1f768760c0b7
Daniel P <danielpimen92@gmail.com>
2018-03-06 13:18:32 -0500

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers(burger_name, devoured)
+VALUES ("Big Mac", false),
+  ("Whooper", false),
+  ("Baconator", false),
+  ("Quarter Pounder", false),
+  ("McRoyal", false),
+  ("Double Double", true);
