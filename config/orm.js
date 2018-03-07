9f8e909f9100ddaf3f2c81d5ce27a5cd2a772336
Daniel P <danielpimen92@gmail.com>
2018-03-07 12:54:56 -0500

    added ORM

---
+++ config/orm.js
@@ -0,0 +1,45 @@
+var connection = require('./connection.js');
+
+connection.connect(function(err){
+	if (err){
+		console.log('Error');
+		return;
+	};
+	console.log('You are connected');
+
+});
+
+var orm = {
+
+	//selectAll
+	selectAll : function(callback){
+		connection.query('SELECT * FROM burgers', function(err, result){
+			if(err) throw err;
+			callback(result);
+		});
+	}, 
+
+	//insertOne
+	insertOne : function(burgerName, callback){
+		connection,query('INSERT INTO burgers SET ?', {
+			burgerName : burgerName,
+			devoured : false
+		}, 
+			function (err, result){
+				if (err) throw err;
+				callback(result);
+		})
+	},
+
+	//updateOne
+	update : function(burgerID, callback){
+		connection.query('UPDATE burgers SET ? WHERE ?', 
+			[{devoured: true}, {id: burgerID}], function(err, result){
+				if (err) throw err;
+				callback(result);
+			});
+	}
+
+}
+
+module.exports = orm;
